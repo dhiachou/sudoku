bbc55eb13ba3cfbc0afcb5e52ed6cb3e78448132
embedMaster <dhiachouchane@gmail.com>
2014-05-14 14:36:28 +0100

    notice is now allocated dynamically

--- src/game.c
+++ src/game.c
@@ -4,7 +4,6 @@
 #define MEDIUM 1
 #define HARD 2
 
-
 #define EASY_LINE_NUM 50
 #define MEDIUM_LINE_NUM 95
 #define HARD_LINE_NUM 11
@@ -25,7 +24,11 @@ int levels_menu()
 Grid main_menu()
 {
     int choice=0, diff=0,n=0;
-    char notice [200]="\0";
+    char *notice =NULL;
+
+    notice = malloc(sizeof(char));
+    notice [0] = '\0';
+
     Grid grid ;
     Vector size;
 
@@ -73,6 +76,7 @@ Grid main_menu()
                 default:
                     fprintf(stderr," Something went wrong \n");
                     delete_grid(&grid);
+                    free(notice);
                     exit(EXIT_FAILURE);
                 break;
             }
@@ -82,14 +86,17 @@ Grid main_menu()
             if (load(&grid) == False ) {
                 fprintf(stderr, "Something went wrong!\n");
                 delete_grid(&grid);
+                free(notice);
                 exit(EXIT_FAILURE);
             }
             break;
         default:
+            notice = realloc(notice, strlen("wrong input \n") * sizeof(char));
             strcpy(notice, "wrong input \n");
             goto menu;
             break;
     }
+    free(notice);
     return grid;
 }
 void game (Grid * grid){
@@ -97,14 +104,17 @@ void game (Grid * grid){
     unsigned short choice = 0;
     Coordinates coord;
     int number=0;
-    char notice [200] = "\0";
+    char notice * = NULL;
+
+    notice = malloc(sizeof(char));
+    notice [0] = '\0';
 
     //In the game
     while (1) {
         clear();
 
         //Displaying last notice :
-        if (strlen(notice) > 2 ){
+        if (strlen(notice) > 0 ){
             printf(" [ ! ] %s ", notice);
             notice[0] = '\0';
         }
@@ -125,7 +135,8 @@ void game (Grid * grid){
         //Testing upon the choice
         switch (choice ){
             case 0 :        /// Quit
-                delete_grid(grid);              // Free allocated memory  TODO !
+                delete_grid(grid);              // Free allocated memory
+                free(notice);
                 exit(EXIT_SUCCESS);             //exit
                 break;
             case 1 :        /// Enter a number
@@ -149,8 +160,10 @@ void game (Grid * grid){
                 //putting the number in the right place if it is a zero (AND EDITABLE! TODO )
                 if (0 == *(grid->Grid[coord.x-1][coord.y-1].val))
                     *(grid->Grid[coord.x-1][coord.y-1].val) = number;
-                else
+                else{
+                    notice = realloc(notice , strlen("field is not empty, or is not editable\n")*sizeof(char));
                     strcpy(notice,"field is not empty, or is not editable\n");
+                }
                 break;
             case 2 :        /// Edit a number
 
@@ -172,11 +185,14 @@ void game (Grid * grid){
                 //putting the number in the right place if it is ( EDITABLE! TODO )
                 if (True)
                     *(grid->Grid[coord.x-1][coord.y-1].val) = number;
-                else
+                else{
+                    notice = realloc(notice , strlen("field is not editable\n")*sizeof(char));
                     strcpy(notice,"field is not editable\n");
+                }
                 break;
             case 3 :        /// Save progress
                     save(*grid);
+                    notice = realloc(notice , strlen("PROGRESS SAVED! \n")*sizeof(char));
                     strcpy(notice,"PROGRESS SAVED! \n");
                 break;
             default :       /// Wrong choice
